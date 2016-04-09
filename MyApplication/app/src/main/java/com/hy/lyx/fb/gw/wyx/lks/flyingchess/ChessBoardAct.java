de1ca88259c80a6c9821c89eff90a037ee2145f2
ksymphony <928540663@qq.com>
2016-04-09 22:35:43 +0800

    frame complete

--- MyApplication/app/src/main/java/com/hy/lyx/fb/gw/wyx/lks/flyingchess/ChessBoardAct.java
+++ MyApplication/app/src/main/java/com/hy/lyx/fb/gw/wyx/lks/flyingchess/ChessBoardAct.java
@@ -2,6 +2,7 @@ package com.hy.lyx.fb.gw.wyx.lks.flyingchess;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class ChessBoardAct extends AppCompatActivity {
 
@@ -9,6 +10,8 @@ public class ChessBoardAct extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess_board);
-        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);//Activity切换动画
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);//Activity切换动画
+        int uiOpts = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOpts);
     }
 }
--- MyApplication/app/src/main/java/com/hy/lyx/fb/gw/wyx/lks/flyingchess/WelcomeAct.java
+++ MyApplication/app/src/main/java/com/hy/lyx/fb/gw/wyx/lks/flyingchess/WelcomeAct.java
@@ -3,18 +3,23 @@ package com.hy.lyx.fb.gw.wyx.lks.flyingchess;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.animation.Animation;
+import android.view.View;
+import android.view.Window;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
 public class WelcomeAct extends AppCompatActivity {
     Timer timer;
+    Timer timer2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //UI setting
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out);//Activity切换动画
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);//Activity切换动画
+        int uiOpts = View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY|View.SYSTEM_UI_FLAG_HIDE_NAVIGATION|View.SYSTEM_UI_FLAG_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN|View.SYSTEM_UI_FLAG_LAYOUT_STABLE|View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
+        getWindow().getDecorView().setSystemUiVisibility(uiOpts);
         //initialization
         timer=new Timer();
         timer.schedule(new TimerTask() {
@@ -22,8 +27,14 @@ public class WelcomeAct extends AppCompatActivity {
             public void run() {
                 Intent intent = new Intent(getApplicationContext(), ChooseModeAct.class);
                 startActivity(intent);
+            }
+        }, 2000);
+        timer2=new Timer();
+        timer2.schedule(new TimerTask() {
+            @Override
+            public void run() {
                 finish();
             }
-        },2000);
+        },4000);
     }
 }
